503a3f18db56efb76352f5e3f50648e5b4d8f039
Backs <rogatnev.sergey@gmail.com>
2018-10-20 19:59:06 +0700

    - comments example

---
+++ CleanCode/CleanCode/Examples/Example17.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CleanCode.Examples
+{
+    class Example17
+    {
+        //список разделителей
+        public static readonly char[] delimiters = { '!', '.', ':', ';', '?', '(', ')' };
+
+        public static List<List<string>> ParseSentences(string text)
+        {
+            var list = new List<List<string>>(); //список слов
+            var sentences = text.Split(delimiters, StringSplitOptions.RemoveEmptyEntries);
+            foreach (string sentence in sentences)
+            {
+                var list2 = GetList(sentence); //получаем список слов
+                if (list2.Count > 0) //если есть словаы
+                {
+                    list.Add(list2);
+                }
+            }
+            return list;
+        }
+
+        public static List<string> GetList(string sentence)
+        {
+            var listWord = new List<string>();
+            var newWord = new StringBuilder();
+            foreach (char checkChar in sentence)
+            {
+                if (char.IsLetter(checkChar) || checkChar == '\'') //проверяем допустимые символы
+                {
+                    newWord.Append(char.ToLower(checkChar)); //переводим символы в верхний регистр
+                }
+                else
+                {
+                    if (newWord.Length != 0)
+                    {
+                        listWord.Add(newWord.ToString());
+                        newWord.Clear();
+                    }
+                }
+            }
+            if (newWord.Length != 0)
+            {
+                listWord.Add(newWord.ToString());
+            }
+            return listWord;
+        }
+    }
+}
---
+++ CleanCode/CleanCode/Examples/Example17_Result.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CleanCode.Examples
+{
+    class Example17_Result
+    {
+        public static readonly char[] delimiters = { '!', '.', ':', ';', '?', '(', ')' };
+
+        public static List<List<string>> ParseSentences(string text)
+        {
+            var sentencesList = new List<List<string>>();
+            var sentences = text.Split(delimiters, StringSplitOptions.RemoveEmptyEntries);
+            foreach (string sentence in sentences)
+            {
+                var listWord = GetListWords(sentence);
+                if (listWord.Count > 0)
+                {
+                    sentencesList.Add(listWord);
+                }
+            }
+            return sentencesList;
+        }
+
+        public static List<string> GetListWords(string sentence)
+        {
+            var listWord = new List<string>();
+            var newWord = new StringBuilder();
+            foreach (char checkChar in sentence)
+            {
+                var isValidChar = char.IsLetter(checkChar) || checkChar == '\'';
+
+                if (isValidChar)
+                {
+                    newWord.Append(char.ToLower(checkChar));
+                }
+                else
+                {
+                    if (newWord.Length != 0)
+                    {
+                        listWord.Add(newWord.ToString());
+                        newWord.Clear();
+                    }
+                }
+            }
+            if (newWord.Length != 0)
+            {
+                listWord.Add(newWord.ToString());
+            }
+            return listWord;
+        }
+    }
+}
